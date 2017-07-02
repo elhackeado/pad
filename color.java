1c5b1b725d4bf13b3c06d161eebb6e36e571b221
El Hackeado <theelhackeado@gmail.com>
2017-07-03 01:10:56 +0530

    Add files via upload

---
+++ color.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class color extends JFrame implements ActionListener
+{
+	JColorChooser jss;
+	color(String s)
+	{
+		super(s);
+		setLayout(new BorderLayout());
+		JPanel jp1 = new JPanel();
+		jp1.setLayout(null);
+		
+		JButton jb1 = new JButton("SET BG");
+		jb1.setBounds(175,505,150,20);
+		jp1.add(jb1);
+		
+		JButton jb2 = new JButton("SET FG");
+		jb2.setBounds(330,505,150,20);
+		jp1.add(jb2);
+		
+		JButton jb3 = new JButton("CANCEL");
+		jb3.setBounds(252,530,150,20);
+		jp1.add(jb3);
+		
+		JColorChooser jss = new JColorChooser();
+		jss.setBounds(0,0,500,500);
+		jp1.add(jss);
+		add(jp1,BorderLayout.CENTER);
+		
+		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+		jb3.addActionListener(this);
+		
+		
+	}
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		String msg = ae.getActionCommand();
+		
+		if(msg.equals("CANCEL"))
+		{
+			setVisible(false);
+		}
+		
+		if(msg.equals("SET BG"))
+		{
+			pad p1 = new pad("");
+			color c1 = new color("");
+			p1.jt1.setBackground(c1.jss.getColor());
+			System.out.println("dsfgsdg");
+			
+		}	
+	}
+	
+	public static void main(String args[])
+	{
+		color c1 = new color("COLOR");
+		
+	}
+}
